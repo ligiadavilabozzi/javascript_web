c119df3806f895af489a438dff607e8740e9a461
ligiadavilabozzi <ligia.bozzi@gmail.com>
2021-05-06 08:01:14 -0300

    Aula 7: Calculando o total

--- app.js
+++ app.js
@@ -18,16 +18,36 @@ incrementa();
 */
 //Só queremos executar a função acima ao clicar no botão
 
-//Chamar o botão incrementa
+//Chama o botão incrementa
 var botaoIncrementa = document.querySelector(".btn-incrementa") //#busca pelo id igual o css
+//Chama o botão incrementa
 var botaoDecrementa = document.querySelector(".btn-decremeta")
-//chama o botão incremeta e adiciona um clique que retorna a função
+//chama o botão incremeta ou decrementa e adiciona um clique que retorna a função
 botaoIncrementa.addEventListener('click',incrementa)
 botaoDecrementa.addEventListener('click',decrementa)
+
+
 function incrementa(){
     
     input.value++
+
+    //pegar o elemento mais próximo (acima)
+var item = botaoIncrementa.closest('.item');
+//buscar no elemento item a classa preco-item
+var precoItem = item.querySelector('.preco-item'); 
+
+//no td quantidade tem um value, já no td preco-item, tem um valor entre as tags <td></td> 
+//temos que pegar esse texto) O number na frente converte texto em numero
+var preco = Number(precoItem.textContent) 
+
+//buscar elemento com id total: 
+var elementoTotal = document.querySelector("#total")
+
+//A partir dele queremos pegar o conteudo e somar com o preço, mas sem apagar o conteúdo dele: 
+elementoTotal.textContent = preco + Number(elementoTotal.textContent)
+
 }
+
 
 function decrementa(){
     input.value--
